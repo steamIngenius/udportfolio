4c99e248e5f0c173a00926990c5a10de49700dc4
Micah Hawkins <micahbhawkins@gmail.com>
2015-05-27 15:19:35 -0700

    Optimize pizza resize

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,14 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    // get mah pizzas
+    var pizzas = document.querySelectorAll(".randomPizzaContainer");
+    // calculate the size of one pizza, then apply to all pizzas!
+    var dx = determineDx(pizzas[0], size);
+    var newwidth = (pizzas[0].offsetWidth + dx) + 'px';
+
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      pizzas[i].style.width = newwidth;
     }
   }
 
